6e9cccd1e497b952ceb233f1c99728a8f805d9bf
anosg93-jpg <anosg93@gmail.com>
2026-07-17 15:08:02 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'taha-elhaty-v1';
+const CACHE_NAME = 'taha-elhaty-v2';
 const assets = [
   './',
   './index.html',
@@ -6,21 +6,31 @@ const assets = [
   './images/logotaha.png'
 ];
 
-// تثبيت السيرفس وركر
+// التثبيت وعمل كاش للملفات الأساسية
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(assets);
-    })
+    }).then(() => self.skipWaiting()) // إجبار التحديث الفوري
   );
 });
 
-// تفعيل السيرفس وركر
+// تفعيل وتحرير الكاش القديم
 self.addEventListener('activate', e => {
-  console.log('Service Worker Activated');
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // الاستحواذ الفوري على الصفحات المفتوحة
+  );
 });
 
-// حدث الـ Fetch الضروري لقبول التثبيت كـ App
+// حدث الـ Fetch الإجباري لقبول تثبيت التطبيق بشكل مستقل
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
